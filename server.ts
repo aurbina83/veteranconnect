0831ec714a17a8dcc71e1506bf295b54af3a125d
aurbina83 <aurbina.metro@gmail.com>
2016-08-04 19:41:37 -0500

    bower fix?

--- server.ts
+++ server.ts
@@ -37,7 +37,7 @@ app.use(helmet());
 app.use('/templates', require('./routes/viewRoutes'));
 app.use('/lib', express.static('./dist'));
 app.use('/lib', express.static('./ngApp'));
-app.use('/scripts', express.static('www/lib'));
+app.use('/scripts', express.static('bower_components'));
 
 app.use('/api/v1/yelp', require('./YelpApi/routes'));
 app.use('/api/v1/users', require('./Users/routes'));
