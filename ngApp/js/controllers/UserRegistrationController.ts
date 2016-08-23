3884e1b37cccb179740760294d8b55752be75c72
Anthony Urbina <aurbina.metro@gmail.com>
2016-08-23 17:26:18 -0500

    merge fix

--- ngApp/js/controllers/UserRegistrationController.ts
+++ ngApp/js/controllers/UserRegistrationController.ts
@@ -9,6 +9,8 @@ namespace app.Controllers {
 
         public branches = ['Army', 'Navy', 'Marines', 'Air Force'];
 
+        // Poop
+
         public email = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
         public password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/;
 
