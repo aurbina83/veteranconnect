7577edccd45194cdfa4d3c4242edda6dffa42d0b
Anthony Urbina <aurbina.metro@gmail.com>
2016-09-14 20:09:55 -0500

    socket on web chat

--- ngApp/js/controllers/EventDetailsController.ts
+++ ngApp/js/controllers/EventDetailsController.ts
@@ -1,17 +1,35 @@
 namespace app.Controllers {
+    declare var io;
     export class EventDetailController {
         public event: app.i.IEvent;
         public comment;
         public status;
         public isLoading = true;
         public attending = [];
+        public socket;
+
+        public socketM() {
+            let name = this.status.name.split(' ');
+            let date = new Date();
+            let message = {
+                event: this.event._id,
+                message: this.comment.message,
+                user: {
+                    firstName: name[0],
+                    lastName: name[1],
+                    imgUrl: this.status.imgUrl
+                },
+                datePosted: date
+            }
+            this.socket.emit('message', message);
+            this.createComment();
+        }
+
 
         public createComment() {
           this.comment.event = this.event._id;
           this.CommentService.create(this.comment).then((res) => {
-            this.event.comments.push(res);
             this.comment.message = "";
-            this.$state.reload();
         }, (err)=>{
             this.ErrorService.toast(err.data.message, "warning");
         });
@@ -31,11 +49,16 @@ namespace app.Controllers {
                 this.event.attending.map((i)=>{
                     this.attending.push(i['_id']);
                 })
+                this.socket = io();
+                this.socket.emit('join', {event: this.event._id});
             }, (err)=>{
                 ErrorService.toast(err.data.message, "warning");
             });
             this.status = UserService.status;
             UserService.userCheck();
+            this.socket.on('receive', (data) =>{
+                this.event.comments.push(data);
+            });
         }
     }
     angular.module('app').controller('EventDetailController', EventDetailController);
