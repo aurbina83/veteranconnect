13e7b0b69e7b585ddf4e4e029c935cc4b21af8bc
Anthony Urbina <aurbina.metro@gmail.com>
2016-09-14 19:44:01 -0500

    Fixed timestamp on comments

--- Comments/model.ts
+++ Comments/model.ts
@@ -4,7 +4,7 @@ export interface ICommentModel extends app.i.IComment, mongoose.Document{}
 
 let commentSchema = new mongoose.Schema({
     message: {type: String, required: true},
-    datePosted: Number,
+    datePosted: {type: Date},
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     event: {type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true}
 });
