f4642970bbb73cf88e40bf99b14e7f64e56b51fe
Anthony Urbina <aurbina.metro@gmail.com>
2016-10-05 19:43:39 -0500

    Changed error message on get all events

--- Events/controller.ts
+++ Events/controller.ts
@@ -46,7 +46,7 @@ export function controller(Event: mongoose.Model<IEventModel>, User: mongoose.Mo
         .populate('eventCreator', 'firstName lastName branch branchImg imgUrl')
         .populate('attending', 'firstName lastName imgUrl branch')
         .exec((err, data) => {
-            if (err) return next({message: "Try reloading the page"});
+            if (err) return next(err);
             res.json(200, data);
         });
     }
