1a4ae64c504deb5340eae43eb9f7b1674a35f931
Anthony Urbina <aurbina.metro@gmail.com>
2016-10-13 17:41:42 -0500

    parse verification result

--- Access/controller.ts
+++ Access/controller.ts
@@ -56,24 +56,24 @@ export function verify(req: express.Request, res: express.Response, next: Functi
         .send(`name=${req.body.name}`)
         .end(function(result) {
             JSON.parse(result.body);
-            console.log(result.body);
-            if (result.body.is_active > 0 || result.body.is_veteran > 0) {
-                User.findOneAndUpdate({ _id: req['payload']._id }, { $set: { verified: true } }, { new: true }, (err, user) => {
-                    if (err) return next({ message: "The verification process is expired or unauthorized" });
-                    let mailOptions = {
-                        from: 'Veteran Connect <info@veteranconnect.co>',
-                        to: user.email,
-                        subject: `Welcome to Veteran Connect, ${user.firstName}!`,
-                        html: welcome(user)
-                    };
-                    transporter.sendMail(mailOptions, (err) => {
-                        if (err) return next(err);
-                        res.json({ token: user.generateJWT() });
-                    })
-                })
-            } else if(result.body.is_active == 0 && result.body.is_veteran == 0) {
-                return next ({message: "Unable to verify your service!"});
-            }
+            res.json(result.body);
+            // if (result.body.is_active > 0 || result.body.is_veteran > 0) {
+            //     User.findOneAndUpdate({ _id: req['payload']._id }, { $set: { verified: true } }, { new: true }, (err, user) => {
+            //         if (err) return next({ message: "The verification process is expired or unauthorized" });
+            //         let mailOptions = {
+            //             from: 'Veteran Connect <info@veteranconnect.co>',
+            //             to: user.email,
+            //             subject: `Welcome to Veteran Connect, ${user.firstName}!`,
+            //             html: welcome(user)
+            //         };
+            //         transporter.sendMail(mailOptions, (err) => {
+            //             if (err) return next(err);
+            //             res.json({ token: user.generateJWT() });
+            //         })
+            //     })
+            // } else if(result.body.is_active == 0 && result.body.is_veteran == 0) {
+            //     return next ({message: "Unable to verify your service!"});
+            // }
         }, function(err) {
             return next (err);
         });
